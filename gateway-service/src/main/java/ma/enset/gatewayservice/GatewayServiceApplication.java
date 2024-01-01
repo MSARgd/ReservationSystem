d814a097d5a4826b8ff3377e66ef88809e1901f0
sidiAbdalla <sidabdallard@gmail.com>
2024-01-01 19:38:44 +0100

    Mon Jan  1 07:38:44 PM +01 2024

--- gateway-service/src/main/java/ma/enset/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/ma/enset/gatewayservice/GatewayServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitio
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.context.annotation.Bean;
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class GatewayServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
